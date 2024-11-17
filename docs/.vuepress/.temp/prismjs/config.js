bd7e1a3fcfcbb488b19f86bcb6d798a788d2e44c
unknown <yu.xiao02@hand-china.com>
2024-11-17 12:10:24 +0800

    [fix]初始化项目

---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,12 @@
+import "D:/blog/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_jkqixjyezz23pgavbquwps2qpm/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "D:/blog/node_modules/.pnpm/@vuepress+plugin-prismjs@2.0.0-rc.60_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_@vu_okksohjg2tcyjiuedohvykq6cy/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "D:/blog/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_jkqixjyezz23pgavbquwps2qpm/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "D:/blog/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_jkqixjyezz23pgavbquwps2qpm/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "D:/blog/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_jkqixjyezz23pgavbquwps2qpm/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "D:/blog/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.56_@vueuse+core@11.2.0_vue@3.5.12__vuepress@2.0.0-rc.18_jkqixjyezz23pgavbquwps2qpm/node_modules/@vuepress/highlighter-helper/lib/client/composables/collapsedLines.js"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
